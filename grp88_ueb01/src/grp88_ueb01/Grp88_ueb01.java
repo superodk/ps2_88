33139a0998ef4bf9d0910b5cba807052efa0b9df
iaw100728 <iaw100728@9d6b248a-a6b4-4b15-8672-4cf2b8dc0735>
2016-05-08 20:44:08 +0000

    rev 0

---
+++ grp88_ueb01/src/grp88_ueb01/Grp88_ueb01.java
@@ -0,0 +1,158 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grp88_ueb01;
+
+/**
+ *
+ * @author ODK
+ */
+public class Grp88_ueb01 {
+
+    /**
+     * verify belonging to prime numbers
+     *
+     * @param examNumber current number in range
+     * @return boolean value
+     */
+    public static boolean isPrim(int examNumber) {
+        // try all values until squareroot of examNumber 
+        for (int counter = 2; counter < Math.sqrt(examNumber) + 1; counter++) {
+            if (examNumber % counter == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * verify belonging to Fibonacci numbers
+     *
+     * @param examNumber current number in range
+     * @return current number belongs to Fibonacci
+     */
+    public static boolean isFibo(int examNumber) {
+        int a = 0, b = 1, c = 0;
+        // Calculate all Fibonacci Numbers until examNumber 
+        while (c < examNumber) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        // Compare if last Fibonacci number equals examNumber 
+        return c == examNumber;
+    }
+
+    /**
+     * verify belonging to Repdigit numbers
+     *
+     * @param examNumber current number in range
+     * @return the current number belongs to Repdigit
+     */
+    public static boolean isRepd(int examNumber) {
+        // at least 2-digits are required
+        if ((examNumber >= -9) && (examNumber <= 9)) {
+            return false;
+        }
+        boolean repdigit = true;
+        int compareValue = examNumber % 10;
+        // compare if the number in every place value equals
+        while (examNumber != 0) {
+            if (compareValue != examNumber % 10) {
+                repdigit = false;
+                break;
+            }
+            // remove last place value of the examNumber
+            examNumber = examNumber / 10;
+        }
+        return repdigit;
+    }
+
+    /**
+     * calculate the Cross Sum
+     *
+     * @param examNumber current number in range
+     * @return calculated Cross Sum
+     */
+    public static int getCsum(int examNumber) {
+        int sum = 0;
+        while (0 != examNumber) {
+            // addiere die letzte ziffer der uebergebenen examNumber zur summe
+            sum = sum + (examNumber % 10);
+            // entferne die letzte ziffer der uebergebenen examNumber
+            examNumber = examNumber / 10;
+        }
+        return sum;
+    }
+
+    /**
+     * verify examNumber is dividable by Cross Sum without remainder
+     *
+     * @param examNumber current number in range
+     * @param sum Cross Sum of examNumber
+     * @return
+     */
+    public static boolean isDivi(int examNumber, int sum) {
+        if (examNumber == 0) {
+            return false;
+        }
+        return (examNumber % sum == 0);
+    }
+// benötigte Spaltenbreite errechnen
+    public static int getNoofChars(int n) {
+        int noofChars = 1;
+
+        if (n < 0) {
+            noofChars++;
+            n = n * (-1);
+        }
+
+        for (int i = 10; i <= n; i *= 10) {
+            noofChars++;
+        }
+        return noofChars;
+    }
+
+    public static int getMaxWidth(int start, int end) {
+        if (getNoofChars(start) > getNoofChars(end)) {
+            return getNoofChars(start);
+        } else {
+            return getNoofChars(end);
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Define a number range 
+        final int minNumber = 11;    // set min range number
+        final int maxNumber = 3100;    // set max range number
+        final int colWidth = getMaxWidth(minNumber, maxNumber);
+
+        // Runs all Numbers through defined range and checks for their properties
+        for (int counter = minNumber; counter < maxNumber; counter++) {
+            // Prime check
+            if (isPrim(counter) == true) {
+                System.out.printf("%" + colWidth + "d: belongs to Prime.\n", counter);
+            }
+            // Fibo check
+            if (isFibo(counter) == true) {
+                System.out.printf("%" + colWidth + "d: belongs to Fibonacci.\n", counter);
+            }
+            // Repd check
+            if (isRepd(counter) == true) {
+                System.out.printf("%" + colWidth + "d: belongs to Repdigit.\n", counter);
+            }
+            // Crosssum check
+            if (isDivi(counter, getCsum(counter)) == true) {
+                System.out.printf("%" + colWidth + "d: is a whole number multiple of the cross sum [%d]\n", counter, getCsum(counter));
+            }
+
+        }
+
+    }
+
+}
