901af08265040b1c313db28ca1e9eec40603176a
iaw100728 <iaw100728@9d6b248a-a6b4-4b15-8672-4cf2b8dc0735>
2016-06-07 08:59:29 +0000

    grp88_ueb04 class Element edited

---
+++ grp88_ueb04/src/grp88_ueb04/Grp88_ueb04.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grp88_ueb04;
+
+/**
+ *
+ * @author iaw100728
+ */
+public class Grp88_ueb04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ grp88_ueb04/src/grp88_ueb04/Set.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grp88_ueb04;
+
+/**
+ *
+ * @author iaw100728
+ */
+public class Set {
+    
+}
